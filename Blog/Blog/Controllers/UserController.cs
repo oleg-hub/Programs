48d25e78c7004c3de759c7226010738fe21f80e8
oleg-hub <vbk80g@mail.ru>
2016-09-25 19:53:39 +0300

    Blog

---
+++ Blog/Blog/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Blog.Models;
+
+namespace Blog.Controllers
+{
+    public class UserController : Controller
+    {
+        BlogContext db = new BlogContext();
+        public ActionResult Index()
+        {
+            return View(db.Articles);
+        }
+        public ActionResult Read(int id)
+        {
+            Article article = db.Articles.FirstOrDefault(a => a.Id == id);
+            return View(article);
+        }
+    }
+}
